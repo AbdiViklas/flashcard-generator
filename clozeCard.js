c58c6af28d1074c2d3a851dd1e826696c2a5b1f3
Andy Bonner <abonner@duke.edu>
2017-07-28 11:53:16 -0400

    build clozeCard constructor

---
+++ clozeCard.js
@@ -0,0 +1,16 @@
+var ClozeCard = function (text, cloze) {
+  if (this instanceof ClozeCard) {
+    if (!text.includes(cloze)) {
+      console.log("Sorry, that cloze text isn't found in the full text! Remember, capatilization counts.");
+    } else {
+      this.fullText = text;
+      this.cloze = cloze.trim();
+      this.partial = text.replace(cloze, "...");
+      console.log(this);
+    }
+  } else {
+    return new ClozeCard(text, cloze);
+  }
+}
+
+module.exports = ClozeCard;
