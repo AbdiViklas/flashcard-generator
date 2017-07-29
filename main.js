c5afc644725d4646bfff94fc70e9b2621659fe76
Andy Bonner <abonner@duke.edu>
2017-07-29 13:46:44 -0400

    build inquirer prompt for basic card

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 var basicCard = require("./basicCard");
 var clozeCard = require("./clozeCard");
+var inquirer = require("inquirer");
 
-var georgeBasic = new basicCard("Who the first prez?", "Georgie W., baby!");
+// Give user choice between basic and cloze-deleted card
+inquirer.prompt([
+  {
+    name: "basicVCloze",
+    type: "rawlist",
+    message: "Which kind of flashcard would you like to create?",
+    choices: ["Basic (Q on front, A on back)", "Cloze-deleted (e.g. '... was the first president')"]
+  }
+])
+.then(answer => {
+  if (answer.basicVCloze === "Basic (Q on front, A on back)") {
+    // if user chooses basic card, get front and back values
+    inquirer.prompt([
+      {
+        name: "front",
+        type: "input",
+        message: "What is the question on the front of the card?"
+      }, {
+        name: "back",
+        type: "input",
+        message: "What is the answer on the back?"
+      }
+    ])
+    .then(answers => {
+      // using the answers, construct a new basic card and log it
+      var newBasic = new basicCard(answers.front, answers.back);
+      console.log(`  BASIC: \n  Front: ${newBasic.front}\n  Back: ${newBasic.back}`)
+    });
+  } else {
+    // if user chooses cloze-deleted card, get values for full text and cloze
+  }
+});
 
-var georgeCloze = clozeCard("George Washington was the first president.", "george Washington");
+// var georgeBasic = new basicCard("Who the first prez?", "Georgie W., baby!");
 
-console.log(`
-  BASIC:
-  Front: ${georgeBasic.front}
-  Back: ${georgeBasic.back}
-`)
+// var georgeCloze = clozeCard("George Washington was the first president.", "george Washington");
 
-console.log(`
-  CLOZE-DEL:
-  Partial: ${georgeCloze.partial}
-  Full: ${georgeCloze.fullText}
-`);
+// console.log(`
+//   CLOZE-DEL:
+//   Partial: ${georgeCloze.partial}
+//   Full: ${georgeCloze.fullText}
+// `);
